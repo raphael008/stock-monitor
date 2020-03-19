e1e5233b209f7e656403ecc8c075a508c54aef45
raphael008 <raphael008@icloud.com>
2020-03-19 18:39:00 +0800

    initial commit

---
+++ src/main/java/com/github/raphael008/stockmonitor/StockMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.raphael008.stockmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMonitorApplication.class, args);
+    }
+
+}
