3ee7b2a1a16f18ed48220ace99b81aa5aa1af28b
raphael008 <raphael008@icloud.com>
2020-03-21 21:19:20 +0800

    add main class & add dependencies

---
+++ stock-monitor-deploy/src/main/java/com/github/raphael008/StockMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.github.raphael008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMonitorApplication.class, args);
+    }
+}
