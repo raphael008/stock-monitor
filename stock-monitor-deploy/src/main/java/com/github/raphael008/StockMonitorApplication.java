5193255b15e48395c0ea27b1040c852e7fe16e8c
raphael008 <raphael008@icloud.com>
2020-04-17 18:26:14 +0800

    resolve dependencies issue

--- stock-monitor-deploy/src/main/java/com/github/raphael008/StockMonitorApplication.java
+++ stock-monitor-deploy/src/main/java/com/github/raphael008/StockMonitorApplication.java
@@ -3,7 +3,7 @@ package com.github.raphael008;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.github.raphael008"})
+@SpringBootApplication
 public class StockMonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockMonitorApplication.class, args);
